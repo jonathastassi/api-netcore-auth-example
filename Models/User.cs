5f58eb9d87675309b43768acf1bd9583614f2799
Jonathas Tassi <jonathas.tassi@datapar.com.br>
2019-11-21 13:42:15 -0300

    Adicionado DataAnnotations na model User para ser usado com o MongoDB

--- Models/User.cs
+++ Models/User.cs
@@ -1,8 +1,17 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
 namespace ShopAuth.Models
 {
     public class User
     {
-        public int Id { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
         public string Username { get; set; }
         public string Password { get; set; }
         public string Role { get; set; }
