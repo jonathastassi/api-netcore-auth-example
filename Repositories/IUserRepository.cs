f4ad96d8742add088565a613236d19561824abcf
Jonathas Tassi <jonathas.tassi@datapar.com.br>
2019-11-21 13:39:06 -0300

    Criado uma interface para fazer a injeção de dependência de UserRepository na HomeController

---
+++ Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using ShopAuth.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopAuth.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> Get(string username, string password);
+    }
+}
